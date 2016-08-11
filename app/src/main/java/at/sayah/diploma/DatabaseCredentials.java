d27d441b2f11ed76115d8970c750fb7e0f9d4312
mreilaender <manuel.reilaender@gmail.com>
2016-08-11 13:03:51 +0200

    add host, database, username, password and port

---
+++ app/src/main/java/at/sayah/diploma/DatabaseCredentials.java
@@ -0,0 +1,12 @@
+package at.sayah.diploma;
+
+/**
+ * @author mreilaender
+ */
+public interface DatabaseCredentials {
+    String HOST = "192.168.100.50";
+    String DATABASE = "mydatabase";
+    String USERNAME = "user";
+    String PASSWORD = "password";
+    int PORT = 3306;
+}
