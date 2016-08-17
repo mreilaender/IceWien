0a6bd8bd0483324e069446f07a820fb4339ed745
mreilaender <manuel.reilaender@gmail.com>
2016-08-17 12:12:05 +0200

    implement parlor list

--- app/src/main/java/at/sayah/diploma/ParlorListView.java
+++ app/src/main/java/at/sayah/diploma/ParlorListView.java
@@ -1,10 +1,14 @@
 package at.sayah.diploma;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import java.util.concurrent.ExecutionException;
 
 public class ParlorListView extends AppCompatActivity {
+
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +16,20 @@ public class ParlorListView extends AppCompatActivity {
         setContentView(R.layout.parlor_list);
 
         ListView parlor_list = (ListView) findViewById(R.id.list_parlor);
+        try {
+            parlor_list.setAdapter(new ParlorAsync(this, parlor_list).execute().get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            System.out.println("");
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
     }
 }
