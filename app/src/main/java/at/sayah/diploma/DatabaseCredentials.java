13ddb4c90b50eafb1799c8c612457edc430deed5
mreilaender <manuel.reilaender@gmail.com>
2016-08-31 02:26:08 +0200

    intellij stuff

--- app/src/main/java/at/sayah/diploma/DatabaseCredentials.java
+++ app/src/main/java/at/sayah/diploma/DatabaseCredentials.java
@@ -4,10 +4,10 @@ package at.sayah.diploma;
  * @author mreilaender
  */
 public interface DatabaseCredentials {
-    String HOST = "192.168.100.50";
-    String DATABASE = "mydatabase";
-    String USERNAME = "user";
-    String PASSWORD = "password";
+    String HOST = "37.221.192.23";
+    String DATABASE = "icewien";
+    String USERNAME = "icewien";
+    String PASSWORD = "icewien";
     int PORT = 3306;
 
     String JDBC_URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
