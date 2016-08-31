9d419872fa8c7e5ab0c4d75a04ebf5817eef6d3a
mreilaender <manuel.reilaender@gmail.com>
2016-08-31 23:56:18 +0200

    remove unnecessary imports

--- app/src/main/java/at/sayah/diploma/FlavourListActivity.java
+++ app/src/main/java/at/sayah/diploma/FlavourListActivity.java
@@ -1,6 +1,5 @@
 package at.sayah.diploma;
 
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
--- app/src/main/java/at/sayah/diploma/entities/Parlor.java
+++ app/src/main/java/at/sayah/diploma/entities/Parlor.java
@@ -3,8 +3,6 @@ package at.sayah.diploma.entities;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
-import java.io.Serializable;
-
 /**
  * @author mreilaender
  */
